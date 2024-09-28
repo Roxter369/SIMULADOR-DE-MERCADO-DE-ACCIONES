0041b185a3289e681cd931b34d36e211dd52b6ad
Lesther Villatoro <rodnyvillatorotzoc@gmail.com>
2024-09-27 21:57:59 -0600

    Modificar y agregar nuevos archivos
    Se modificó el main y se agregó otro archivo llamado order.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,21 @@
-export class Order {
-    company: string;
-    quantity: number;
-    price: number;
-    isBuyOrder: boolean;
+import { Order } from './order';
+import { MaxHeap } from './maxHeap';
+import { MinHeap } from './minHeap';
 
-    constructor(company: string, quantity: number, price: number, isBuyOrder: boolean) {
-        this.company = company;
-        this.quantity = quantity;
-        this.price = price;
-        this.isBuyOrder = isBuyOrder;
-    }
-}
+const buyOrders = new MaxHeap(10);
+const sellOrders = new MinHeap(10);
+
+// Órdenes de compra
+buyOrders.insert(new Order('CompanyA', 100, 50, true));
+buyOrders.insert(new Order('CompanyA', 50, 55, true));
+
+// Órdenes de venta
+sellOrders.insert(new Order('CompanyA', 100, 45, false));
+sellOrders.insert(new Order('CompanyA', 50, 40, false));
+
+// Mejor orden de compra y venta
+const bestBuyOrder = buyOrders.getMax();
+const bestSellOrder = sellOrders.getMin();
+
+console.log('Best Buy Order:', bestBuyOrder);
+console.log('Best Sell Order:', bestSellOrder);
